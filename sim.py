abb5c62147aac6cd6994e3f6eb6a0a4d460b786c
Tom <tombrandisicloud@gmail.com>
2024-03-22 13:39:02 +0000

    planet physics and orbit line

--- sim.py
+++ sim.py
@@ -31,15 +31,14 @@ class vector:
         return None
 
 class planet:
-    def __init__(self, name:str, mass:int, radius:int, position:tuple, next:tuple=(0,0)) -> None:
+    def __init__(self, name:str, mass:int, radius:int, position:tuple, velocity:tuple=(0,0)) -> None:
         self.name = name
         self.mass = mass
         self.position = position
         self.radius = radius
-        self.next = next
+        self.velocity = velocity
 
-def draw(planets:slice):
-    turtle_spaceship.clear()
+def draw_planets(planets:slice):
     turtle_planet.clear()
     for planet in planets:
         turtle_planet.penup()
@@ -49,12 +48,15 @@ def draw(planets:slice):
         turtle_planet.setposition(planet.position[0], planet.position[1]-(1*planet.radius))
         turtle_planet.pendown()
         turtle_planet.circle(planet.radius)
+
+def draw_ship():
+    turtle_spaceship.clear()
     turtle_spaceship.setposition(spaceship_pos)
     turtle_spaceship.dot(10)
 
 def move_planets(planets):
     for planet in planets:
-        planet.position = (planet.position[0] + planet.next[0], planet.position[1] + planet.next[1])
+        planet.position, planet.velocity = move_spaceship(planet.position, planet.velocity, planets)
 
 def calculate_overall_force(spaceship_pos, planets):
     overall_force = vector((0,0))
@@ -69,18 +71,37 @@ def move_spaceship(spaceship_pos, spaceship_vel, planets):
     spaceship_pos = (spaceship_pos[0] + spaceship_vel[0], spaceship_pos[1] + spaceship_vel[1])
     return spaceship_pos, spaceship_vel
 
-spaceship_pos = (130,100)
-spaceship_vel = (0,-25)
+def draw_orbit(spaceship_pos, spaceship_vel, planets, steps):
+    turtle_orbit.clear()
+    turtle_orbit.setposition(spaceship_pos)
+    turtle_orbit.pendown()
+    temp_vel = spaceship_vel
+    temp_pos = spaceship_pos
+    i=0
+    while i < steps:
+        i+=1
+        temp_pos, temp_vel = move_spaceship(temp_pos, temp_vel, planets)
+        turtle_orbit.goto(temp_pos)
+    turtle_orbit.penup()
+
+spaceship_pos = (200,0)
+spaceship_vel = (0,-7)
 
 turtle_planet = turtle.Turtle(visible=False)
 turtle_planet.speed(0)
+turtle_orbit = turtle.Turtle(visible=False)
+turtle_orbit.speed(0)
+turtle.tracer(10)
 turtle_spaceship = turtle.Turtle(visible=False)
 turtle_spaceship.speed(0)
-#planets.append(planet("mars", 0.01, 50, (150,50)))
-planets.append(planet("earth", 0.01, 100, (0,0), next=(0,0) ) )
-draw(planets)
+turtle_orbit.tracer = False
+planets.append(planet("one", 0.001, 50, (0,100), (3, 0)))
+planets.append(planet("two", 0.001, 50, (0,0) , (-3, 0)) )
+draw_planets(planets)
+
 while True:
-    time.sleep(0.1)
     move_planets(planets)
     spaceship_pos, spaceship_vel = move_spaceship(spaceship_pos, spaceship_vel, planets)
-    draw(planets)
+    draw_orbit(spaceship_pos, spaceship_vel, planets, 2000)
+    draw_ship()
+    draw_planets(planets)
